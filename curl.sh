110880dd54b30b0a9dc934e88050ac05212123b1
Nicolas Nys <nicolas.nys@epita.fr>
2021-10-06 14:44:53 +0200

    add mdoif

--- curl.sh
+++ curl.sh
@@ -2,11 +2,8 @@
 
 
 curl -i -X POST \
-  --url http://localhost:8001/services/ \
-  --data 'name=stokomani' \
-  --data 'url=https://lb-api-prod.intranet.stokomani.fr/stokomani/carrier/declaration/ping' 
+  --url http://localhost:8001/consumers/ \
+  --data 'username=technical_account';
 
-curl -i -X POST \
-  --url http://localhost:8001/services/stokomani/routes \
-  --data 'name=stokomani-route' \
-  --data 'paths[]=/stokomani/carrier/declaration/ping'
+curl -i -s -o -X POST \
+  --url http://localhost:8001/consumers/technical_account/key-auth;
