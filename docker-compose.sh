4717babe0e9556bee89bc8b5dd43fe5a309436b9
Nicolas Nys <nicolas.nys@epita.fr>
2021-10-06 10:10:19 +0200

    [DOC]

--- docker-compose.sh
+++ docker-compose.sh
@@ -5,12 +5,12 @@
 #Install Docker
 
 sudo apt update;
-sudo apt install apt-transport-https ca-certificates curl software-properties-common;
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common;
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable";
 sudo apt update;
 apt-cache policy docker-ce;
-sudo apt install docker-ce;
+sudo apt install -y docker-ce;
 docker --version;
 
 #Install Docker Compose
