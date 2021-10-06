1ccd2942406992c1a19a67f9201b8bd8ed49f5a1
Nicolas Nys <nicolas.nys@epita.fr>
2021-10-06 12:52:18 +0200

    ad

---
+++ curl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+curl -i -X POST \
+  --url http://localhost:8001/services/ \
+  --data 'name=stokomani' \
+  --data 'url=https://lb-api-prod.intranet.stokomani.fr/stokomani'
+
+curl -i -X POST \
+  --url http://localhost:8001/services/stokomani/routes \
+  --data 'name=stokomani-route' \
+  --data 'paths[]=/stokomani/carrier/declaration/ping'
